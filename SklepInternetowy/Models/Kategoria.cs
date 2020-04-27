a2b7c3d54bcb1f89a57dc043c44d934e844d66a0
Adam Terebiński <aterebinski@wp.pl>
2020-04-27 14:02:47 +0200

    Dodanie atrybutów do klas modeli

--- SklepInternetowy/Models/Kategoria.cs
+++ SklepInternetowy/Models/Kategoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace SklepInternetowy.Models
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategorii { get; set; }
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
         public virtual ICollection<Kurs> Kursy { get; set; }
