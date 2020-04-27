b3d97cf3d6e2aae3b6d268148a8586ce47ab2f9b
Adam Terebiński <aterebinski@wp.pl>
2020-04-27 13:23:00 +0200

    Dodanie klas modelu dla aplikacji

---
+++ SklepInternetowy/Models/Kategoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+        public virtual ICollection<Kurs> Kursy { get; set; }
+    }
+}
